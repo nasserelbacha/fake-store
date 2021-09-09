e8620d096bb3a08aba403f1d21d15755e817ba4e
nasser el bacha <nasseramelbacha2002@gmail.com>
2021-09-09 13:54:05 -0300

    page done

---
+++ src/components/ProductListing.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useCallback, useMemo } from "react";
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import {setProducts} from "../redux/actions/productActions"
+import ProductComponent from './ProductComponent';
+const ProductPage= () =>{
+    const products = useSelector((state) => state.allProducts.products);
+    const dispatch = useDispatch();
+    const fetchProducts = async () => {
+      const response = await axios
+        .get("https://fakestoreapi.com/products")
+        .catch((err) => {
+          console.log("Err: ", err);
+        });
+      dispatch(setProducts(response.data));
+    };
+  
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+
+    console.log("Products :", products);
+
+    return(
+    <div className="ui grid container">
+       <ProductComponent></ProductComponent>
+    </div> 
+    ) 
+}
+export default ProductPage;
